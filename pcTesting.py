f48bf29306e549f1d0b84a0cda22ba3215f50253
4.29.25-Final-Project-NNWebViz-F <willwimbiscus@gmail.com>
2025-05-22 11:47:59 -0400

    floating point work

--- pcTesting.py
+++ pcTesting.py
@@ -7,6 +7,11 @@ def tick_clock():
 
     current_cycle += 1
 
+def halfAdder(a, b):
+    s = a ^ b
+    cout = a & b
+    return s, cout
+
 def fullAdder(a, b, c):
     x1 = a ^ b
     s = x1 ^ c
@@ -23,42 +28,218 @@ def bitAdd32(a, b, c_in):
         s[i], c[i + 1] = fullAdder(a[i], b[i], c[i])
     return s, c[BIT_SIZE]
 
+
+
+#used for bitMul32
+def bitAdd64(a, b, c_in):
+    bitSize2 = 2*BIT_SIZE
+    s = [False] * bitSize2
+    c = [False] * (bitSize2+1)
+    c[0] = c_in
+    for i in range(bitSize2):
+        s[i], c[i + 1] = fullAdder(a[i], b[i], c[i])
+    return s, c[bitSize2]
+
 def bitSub32(a, b):
     # Two's complement subtraction: a - b = a + (~b + 1)
     nb = [not b[i] for i in range(BIT_SIZE)]
     s, cout = bitAdd32(a, nb, True)
     return s, cout
 
+def bitMul4(a, b):
+    p = [[a[i] & b[j] for j in range(4)] for i in range(4)]
+    p00, p01, p02, p03 = p[0]
+    p10, p11, p12, p13 = p[1]
+    p20, p21, p22, p23 = p[2]
+    p30, p31, p32, p33 = p[3]
+
+    s1, c1 = halfAdder(p10, p01)
+    s2, c2 = fullAdder(p20, p11, p02)
+    s3, c3 = fullAdder(p30, p21, p12)
+    s3b, c3b = halfAdder(s3, c1)
+    s4, c4 = fullAdder(p31, p22, p13)
+    s5, c5 = halfAdder(p32, p23)
+
+    rowA = [p00, s1, s2, s3b, s4, s5]
+    rowB = [0, 0, c2, c3, c4, c5]
+
+    out, carry = [], 0
+    for a_bit, b_bit in zip(rowA, rowB):
+        s, carry = halfAdder(a_bit ^ carry, b_bit)
+        out.append(s)
+    out.append(carry)
+    return out + [0, 0]  # 8-bit product, LSB first
+
+
+
+# Example usage:
+# Multiply 6 (0110) × 11 (1011)
+a = [0,1,1,0]
+b = [1,1,0,1] 
+print("4bit multiplier:",bitMul4(a, b))  # Should return 66 
+
 #32bit multiplier
 def bitMul32(a, b):
+    outputArr = [[False,False] for _ in range(32)]
+    a=[]
+    b=[]
+    # No, this does not work as intended.
+    # [[]*32 for _ in range(32)] creates a list of 32 empty lists, because [] * 32 is still [].
+    # If you want a 32x32 array (list of lists), use:
+    partialProducts = [[False for _ in range(32)] for _ in range(32)]
+    #simulatable. 32bit in typical partial product style would be 1024 insanity and the HA/FA optimization would be too much
+    #could loop over --> 
+    counter = 0
+    for i in range(32):
+        for j in range(32):
+            partialProducts[i][j] = a[i] & b[j]  # This evaluates to either True/False (if a[i], b[j] are bools) or 1/0 (if ints)
+    #peaks at a15*b0 or a0*b15 --> precisely 32 partial products
+    #something like this repeated for multiplication
+    for i in range(15):
+        if counter == 0:
+            outputArr[0][0] = partialProducts[0][0]
+        elif counter == 1:
+            outputArr[1][0], outputArr[1][1] = partialProducts[1][0], partialProducts[0][1]
+        elif counter == 2:
+            sum, cout = halfAdder(partialProducts[1][1], partialProducts[0][2]) #can't do this, need to pass cout between columns etc
+            outputArr[2][0], outputArr[2][1] = partialProducts[2][0], sum
+        elif counter == 3:
+            pass
+        elif counter == 4:
+            pass
+        elif counter == 5:
+            pass
+        elif counter == 6:
+            pass
+        elif counter == 7:
+            pass
+        elif counter == 8:
+            pass
+        elif counter == 9:
+            pass
+        elif counter == 10:
+            pass
+        elif counter == 11:
+            pass
+        elif counter == 12:
+            pass
+        elif counter == 13:
+            pass
+        elif counter == 14:
+            pass
+            
+        print("counter:", counter)
+        counter+=1
+
+    for f,g in zip(outputArr):
+        a.append(f)
+        b.append(g)
+
+
+
+# # #32bit divider
+# def bitDiv32(a, b):
+
+def bitAdd16(a, b, c_in):
+    s = [False] * 16
+    c = [False] * (16+1)
+    c[0] = c_in
+    for i in range(16):
+        s[i], c[i + 1] = fullAdder(a[i], b[i], c[i])
+    return s, c[16]
+
+def bitAdd5(a, b, c_in):
+    s = [False] * 5
+    c = [False] * (5+1)
+    c[0] = c_in
+    for i in range(5):
+        s[i], c[i + 1] = fullAdder(a[i], b[i], c[i])
+    return s, c[5]
+
+# def bitFPAdd32(a, b):
+
+# def bitFPSub32(a, b):
+
+fpa = [False,False,False,False,True,True,True,False,False,False,False,False,False,True,True,True] 
+#0 is sign bit, 00011 (exponnent), #(implicit 1 is added here)11000000111 is mantissa.
+#this value is 1.1000000111 * 2^3 = 12.0546875...
+fpb = [False,True,False,True,True,True,True,False,True,False,False,False,False,True,False,True]
+#0 is sign bit, 10111 (exponent), #(implicit 1 is added here)1010000101 is mantissa. 
+#this value is 1.1010000101 * 2^8 = 417.25
+#output should be ~4900-5000
+
+def bitMul11(a, b):
     
+    partialProducts = [[False for _ in range(11)] for _ in range(11)]
+    sum2Column = [[False,False] for _ in range(22)]
+    #we'll do 22 'columns' of partial products
+    
+    ## --- just defining some intermittent variables:
+    sum1,cout1 = False,False
+    sum2,cout2 = False,False
+    sum3,cout3 = False,False
+    sum4,cout4 = False,False
+    sum5,cout5 = False,False
+    sum6,cout6 = False,False
+    sum7,cout7 = False,False
+    sum8,cout8 = False,False
+    sum9,cout9 = False,False
+    
+    ## ---
+    phase=1
+    if(phase==1):#0-1
+        sum2Column[0][0] = a[0] & b[0] #lsbs
+    elif(phase==2):#0/2
+        sum2Column[1][0],sum2Column[1][1] = (a[1]&b[0]),(a[0]&b[1])
+    elif(phase==3):#0/4
+        sum1,cout1 = halfAdder((a[2]&b[0]),(a[1]&b[1]))
+        sum2Column[2][0],sum2Column[2][1] = sum1,(a[0]&b[2])
+    elif(phase==4):#0/8
+        sum2,cout2 = halfAdder((a[3]&b[0]),(a[1]&b[2]))
+        sum3,cout3 = fullAdder((a[2]&b[1]),(a[0]&b[3]),cout1)
+        sum2Column[3][0],sum2Column[3][1] = sum2,sum3
+    elif(phase==5):#0/16 --> 2 FAs and 1 HA
+        sum4,cout4 = 
+    elif(phase==6):#0/32 --> 3 FAs and 1 HA
+    
+    elif(phase==7):#0/64 --> 4 FAs and 1 HA
+
+    elif(phase==8):#0/128 --> 5 FAs and 1 HA
+
+    elif(phase==9):#0/256 --> 
+
+def bitFPMul16(a, b):
+    #a and b are 16bit floats
+    partialProducts = [[False for _ in range(10)] for _ in range(10)]
+    #negate. We use -15? [True,False,False,False,True] (msb is last array index)
+    bitNeg15 = [True,False,False,False,True]
+    interimA = bitAdd5(a,bitNeg15,True)
+    interimB = bitAdd5(b,bitNeg15,True)
+    outB = bitAdd5(interimA,interimB,False)
+
+    #multiply the 10bit mantissas (actually 11bit via implicit 1s) to get 22bit product
+
+
+    return outB
+    #specific 
+    #multiplication
 
 
 
-#32bit divider
-def bitDiv32(a, b):
 
 
+# def bitFPDiv32(a, b):
 
+# # #32bit shifter
+# def bitShifter32():
 
-def bitFPAdd32(a, b):
+# def bitAND32(a, b):
 
-def bitFPSub32(a, b):
+# def bitOR32(a, b):
 
-def bitFPMul32(a, b):
+# def bitXOR32(a, b):
 
-def bitFPDiv32(a, b):
-
-# #32bit shifter
-def bitShifter32():
-
-def bitAND32(a, b):
-
-def bitOR32(a, b):
-
-def bitXOR32(a, b):
-
-def bitNOT32(a):
+# def bitNOT32(a):
 
 
 
@@ -93,6 +274,16 @@ def reverseArray(r):
     return new_arr
 
 def testTwoNums():
+    
+    # 32-bit value: alternating False/True
+    multt1 = [False]*3 + [True]*6 + [False]*23 #504
+    multt2 = [False]*3 + [True]*6 + [False]*23 #504
+    print(f"multt1 (decimal): {convertToDecimal(convert_bool_to_binary(multt1))}")
+    print(f"multt2 (decimal): {convertToDecimal(convert_bool_to_binary(multt2))}")
+    #calculate
+    # result_mult = bitMul32(multt1, multt2)
+    # print(f"Binary product: {convertToDecimal(convert_bool_to_binary(result_mult))}")
+    # print(f"Human readable product: {convertToString(reverseArray(convert_bool_to_binary(result_mult)))}")
     # 32-bit value: alternating False/True
     subtract1 = [True]*32 #-1
     subtract2 = [True]*1+[False]*28+[True]*3 #1
@@ -122,7 +313,7 @@ def testTwoEnteredNums():
     if test == 'y':
         num1 = input("enter first 32 bit number in decimal")
         num2 = input("enter second 32 bit number in decimal")
-        result_sum, carry_out = bitAdder32(num1, num2, False)
+        result_sum, carry_out = bitAdd32(num1, num2, False)
         print(f"Binary sum: {convertToDecimal(convert_bool_to_binary(result_sum))}, Carry out: {carry_out}")
         print(f"Human readable sum: {convertToString(reverseArray(convert_bool_to_binary(result_sum)))}")
 
